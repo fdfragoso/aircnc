6f49c9f66022de17e382f220fe6d7a54a321563c
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-02-26 16:58:09 +0100

    feat #9: Add routes to react

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,3 +1,20 @@
+//VIDEO STOPED ON 53MIN
+
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import 
+import Login from './pages/Login';
+import New from './pages/New';
+import Dashboard from './pages/Dashboard';
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Login} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/new" component={New} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
