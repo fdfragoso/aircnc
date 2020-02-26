8aae987400a66816e3fa0b8a64f80ab0225a79c4
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-02-26 09:27:33 +0100

    feat #1: Add route - return spot by user

--- Backend/src/controllers/SpotController.js
+++ Backend/src/controllers/SpotController.js
@@ -1,8 +1,16 @@
 const Spot = require('../models/Spot');
 const User = require('../models/User');
 
-
 module.exports = {
+    
+    async index(req, res){
+        const { tech } = req.query;
+
+        const spots = await Spot.find({ techs: tech });
+
+        return res.json(spots);
+    },
+        
     async store(req, res){
 
         const { filename } = req.file;
