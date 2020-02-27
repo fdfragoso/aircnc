3aa9906fdd94473e7b265ce21aab7f3704b694b5
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-02-27 18:04:39 -0300

    update #1: dashboard.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../../services/api'
 
-export default function Dashboard(){
-    return <h1>Dashboard</h1>
+export default function Dashboard() {
+    useEffect( () => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard');
+        }
+        loadSpots();
+    }, [])
+    return
 }
