540cddf876b58960ba3d7f9c91567449c38c2343
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-02-27 17:43:47 -0300

    struct #2: Move code from 'app.js' to 'login/index.js'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
-import api from './services/api'
-
 import logo from './assets/logo.svg';
+import Routes from './routes'
 
 function App() {
-  const [email, setEmail] = useState('');
 
-  async function handleSubmit(event) {
-    event.preventDefault()
-
-    const response = await api.post('/sessions', { email });
-
-    const { _id } = response.data;
-   
-    localStorage.setItem('user', _id);
 
   }
   return (
@@ -22,22 +12,8 @@ function App() {
       <img src={logo} alt="AirCnC" />
 
       <div className="content">
-        <p>
-          Offer <strong>seats</strong> to programmers and find <strong>talents</strong>!
-       </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Your email"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button type="submit" className="btn">Enter</button>
-        </form>
+        <Routes />
+       
       </div>
     </div>
   );
