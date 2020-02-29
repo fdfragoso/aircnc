57fa7ccd8d52e9613aa1d47d76e33c2d83ca706e
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-02-29 17:13:29 +0100

    feat #13: Add new page to add places

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api'
 
 import './styles.css';
@@ -30,6 +31,9 @@ export default function Dashboard() {
                     </li>
                 ))}
             </ul>
+            <Link to="/new">
+                <button className="btn">Register new place</button>
+            </Link>
         </>
     )
 }
--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import camera from '../../assets/camera.svg';
+import './styles.css'
 
 export default function New(){
-    return <div />
+
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+
+    function handleSubmit(){
+
+    }
+    
+    return (
+        <form onSubmit={handleSubmit}>
+        <label id="thumbnail">
+            <input type="file" />
+            <img src={camera} alt="Select image"/>
+        </label>
+        
+        <label htmlFor="company">COMPANY *</label>
+        <input 
+            id="company"
+            placeholder="Your amazing company"
+            value={company}
+            onChange={event => setCompany(event.target.value)}
+        />
+
+        <label htmlFor="techs">TECHNOLOGIES *<span> (split by comma)</span></label>
+        <input 
+            id="techs"
+            placeholder="Which techs are used?"
+            value={techs}
+            onChange={event => setCompany(event.target.value)}
+        />
+
+        <label htmlFor="price">DAILY PRICE *<span> (if free leave blank)</span></label>
+        <input 
+            id="price"
+            placeholder="Daily price?"
+            value={price}
+            onChange={event => setPrice(event.target.value)}
+        />
+
+        <button type="submit" className="btn">Register</button>
+        </form>
+    )
 }
