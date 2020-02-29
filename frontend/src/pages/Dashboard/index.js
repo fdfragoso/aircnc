f26ed23d89a6b63039985aea171568bf220369ac
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-02-29 16:41:40 +0100

    feat #12: Add style to dashboard page

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api'
 
+import './styles.css';
 export default function Dashboard() {
     const [spots, setSpots] = useState([]);
     
@@ -22,9 +23,10 @@ export default function Dashboard() {
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
-                        <header />
+                        <header style={{backgroundImage: 
+                         `url(${spot.thumbnail_url})`}}/>
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                     </li>
                 ))}
             </ul>
