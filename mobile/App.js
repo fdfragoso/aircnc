c052834aa0bbbae687ab2f0e7d11ba893f882fca
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-19 00:07:51 +0100

    Create routes
    Naviagtion pages

--- mobile/App.js
+++ mobile/App.js
@@ -1,24 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import Routes from './src/routes';
+ 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World! Omnistack</Text>
-      <Text style={styles.text}>Hello World! Omnistack</Text>
-    </View>
-  );
+  return
+    <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  text: {
-    fontWeight: 'bold',
-    color: 'red'
-  }
-});
---
+++ mobile/src/routes.js
@@ -0,0 +1,15 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Book from '.T/pages/Book';
+import Login from './pages/Login';
+import List from './pages/List';
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Login,
+        List,
+        Book
+    })
+);
+
+export default Routes;
