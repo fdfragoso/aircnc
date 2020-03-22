56f231992b5567f292e5b5aa163d478c050b70f2
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-03-22 22:21:17 +0100

    Update List.js
    add logo and style

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,35 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import logo from '../assets/logo.png'; 
 
 export default function List(){
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storageTechs => {
+            const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray); 
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    },
+});
